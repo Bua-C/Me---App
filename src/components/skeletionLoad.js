3d07dd16f46c1de2e8b56b44e61d4b30af963447
lathsamee <giftsforam@gmail.com>
2020-09-25 07:35:27 +0700

    Add Card Animation

--- src/components/skeletionLoad.js
+++ src/components/skeletionLoad.js
@@ -1,11 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import {TouchableOpacity} from 'react-native';
-import {View, Text, Animated, Easing} from 'react-native';
+import {View, Text, Animated, Easing, StyleSheet} from 'react-native';
 
 const SkeletionLoading = props => {
   // *@ State
   const opacityValue = new Animated.Value(0);
-  const offsetX = new Animated.Value(0);
+  const offsetX = new Animated.Value(-100);
   const [loading, setLoading] = useState(true);
 
   // console.log(opacityValue.addListener(({value}) => console.log(value)));
@@ -25,7 +25,7 @@ const SkeletionLoading = props => {
 
   const _opacityView = () => {
     // alert('KJLKJL');
-    opacityValue.setValue(0);
+    opacityValue.setValue(-100);
     Animated.timing(opacityValue, {
       toValue: 2,
       duration: 1000,
@@ -35,7 +35,7 @@ const SkeletionLoading = props => {
   const moveToLeft = () => {
     offsetX.setValue(0);
     Animated.timing(offsetX, {
-      toValue: 430,
+      toValue: 414,
       duration: 1000,
       easing: Easing.linear,
       useNativeDriver: true,
@@ -68,25 +68,74 @@ const SkeletionLoading = props => {
           borderRadius: 20,
         }}
       />
-      <Animated.View
+
+      <View
         style={{
-          backgroundColor: 'red',
-          width: 100,
-          height: 100,
-          borderRadius: 20,
-          transform: [{translateX: offsetX}],
-          opacity: offsetX.interpolate({
-            inputRange: [0, 400],
-            outputRange: [0.8, 0],
-          }),
-        }}
-      />
-      <TouchableOpacity
-        onPress={() => movingBox}
-        style={{backgroundColor: 'blue', padding: 10}}>
-        <Text>Moving</Text>
-      </TouchableOpacity>
+          backgroundColor: '#ffffff',
+          padding: 10,
+          marginHorizontal: 10,
+          flexDirection: 'row',
+          marginTop: 10,
+          borderRadius: 5,
+          justifyContent: 'space-between',
+          ...styles.shadow,
+        }}>
+        <View
+          style={{
+            backgroundColor: '#c1c0c0',
+            width: 100,
+            height: 100,
+            justifyContent: 'flex-start',
+            borderRadius: 100,
+            zIndex: 100,
+          }}>
+          <Animated.View
+            style={{
+              backgroundColor: '#ffffff',
+              width: 50,
+              height: 100,
+              transform: [{translateX: offsetX}],
+              opacity: offsetX.interpolate({
+                inputRange: [0, 450],
+                outputRange: [0.8, 0],
+              }),
+            }}
+          />
+        </View>
+        <View style={{width: '70%', marginTop: 20}}>
+          <View
+            style={{
+              backgroundColor: '#c1c0c0',
+              width: '50%',
+              height: 30,
+              borderRadius: 15,
+            }}
+          />
+          <View
+            style={{
+              backgroundColor: '#c1c0c0',
+              marginTop: 15,
+              width: '90%',
+              height: 30,
+              borderRadius: 15,
+            }}
+          />
+        </View>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  shadow: {
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.32,
+    shadowRadius: 5.46,
+
+    elevation: 9,
+  },
+});
 export default SkeletionLoading;
